b4d5cf0c91f0e684a677f1e836e2f7d459e917e0
maxmwang <orangemax888@gmail.com>
2024-06-16 00:12:11 -0700

    parallelize scraping

--- internal/scrape/daemon.go
+++ internal/scrape/daemon.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"slices"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/maxmwang/jobet/internal/db"
@@ -14,12 +15,10 @@ import (
 
 func Daemon(ctx context.Context, q *db.Queries) {
 	t := time.NewTicker(10 * time.Minute)
-	count := 0
+	count := 144
 
 	for {
-		count++
-		priority := 1
-
+		var priority int64
 		if count%144 == 0 { // 1d
 			priority = 5
 			count = 0 // reset to prevent overflow
@@ -29,25 +28,46 @@ func Daemon(ctx context.Context, q *db.Queries) {
 			priority = 3
 		} else if count%2 == 0 { // 20min
 			priority = 2
+		} else {
+			priority = 1
 		}
 
 		fmt.Printf("[%s] scraping priority<=%d\n", time.Now().Format(time.TimeOnly), priority)
 
-		companies, err := q.GetCompaniesByMaxPriority(ctx, int64(priority))
+		companies, err := q.GetCompaniesByMaxPriority(ctx, priority)
 		if err != nil {
 			log.Printf("failed to get companies from database: %v", err)
 			continue
 		}
 
-		sortedJobs := make([]output.Job, 0)
+		wgScrape := new(sync.WaitGroup)
+		jobsChan := make(chan []output.Job)
 		for _, c := range companies {
-			jobs, err := scrape(c)
-			if err != nil {
-				log.Printf("failed to scrape company: %s", err.Error())
-				continue
-			}
-			sortedJobs = append(sortedJobs, jobs...)
+			wgScrape.Add(1)
+			go func() {
+				defer wgScrape.Done()
+				jobs, err := scrape(c)
+				if err != nil {
+					log.Printf("failed to scrape company: %s", err.Error())
+				}
+				jobsChan <- jobs
+			}()
 		}
+
+		wgCollect := new(sync.WaitGroup)
+		sortedJobs := make([]output.Job, 0)
+		wgCollect.Add(1)
+		go func() {
+			defer wgCollect.Done()
+			for jobs := range jobsChan {
+				sortedJobs = append(sortedJobs, jobs...)
+			}
+		}()
+
+		wgScrape.Wait()
+		close(jobsChan)
+		wgCollect.Wait()
+
 		slices.SortFunc(sortedJobs, func(a, b output.Job) int {
 			return strings.Compare(a.UpdatedAt.String(), b.UpdatedAt.String())
 		})
@@ -55,5 +75,6 @@ func Daemon(ctx context.Context, q *db.Queries) {
 		output.Log(sortedJobs)
 
 		<-t.C
+		count++
 	}
 }
